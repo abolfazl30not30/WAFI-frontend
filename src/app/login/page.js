47d8cd47ab597612ad8cc8d1ed6af6b143925177
Abolfazl <abolfazl30not30@gmail.com>
2023-09-21 02:19:36 +0330

    dd

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -6,7 +6,7 @@ const Login = () => {
     const clientId =
       "188888100839-1840iic8vn85kj85a28gqcn92262lblm.apps.googleusercontent.com";
     const clientSecret = "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";
-    const redirectUri = "https://wafi-frontend-jocu.vercel.app/redirect";
+    const redirectUri = "http://localhost:3000/redirect";
     const authUrl = `https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=email%20profile`;
 
     window.location.href = authUrl;
